29443bfd8f7c23c68a020f712e3f4dd5dabf5876
Harindu Indeewara Abhayathunge <99375057+IT21250842@users.noreply.github.com>
2022-05-20 14:54:26 +0530

    Tickets.cpp

---
+++ Tickets.cpp
@@ -0,0 +1,51 @@
+#include "Tickets.h"
+
+Ticket::Ticket()
+{
+	TicketID = 0;
+	TicketType = "";
+	TicketCategory = "";
+	SeatNo = 0;
+	book[0] = new Booking(0);
+	book[1] = new Booking(0);
+}
+
+Ticket::Ticket(int b1, int b2)
+{
+	book[0] = new Booking(b1);
+	book[1] = new Booking(b2);
+}
+
+void Ticket::setTicketDeatils(int pid, string ptype, string pcategory, int ps_no)
+{
+	
+}
+
+int Ticket::getTicketID()
+{
+	
+}
+
+string Ticket::getTicketType()
+{
+	
+}
+
+string Ticket::grtTicketCategory()
+{
+	
+}
+
+int Ticket::getSeatNo()
+{
+	
+}
+
+Ticket::~Ticket()
+{
+	for (int i=0 ;i < SIZE; i++)
+    {
+      delete book[i];
+    }
+}
+
