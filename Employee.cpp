7f8540d1ba971d5d0087251c17245fbc3bea913f
Nirmal Widanage <99357825+IT21377426@users.noreply.github.com>
2022-05-20 14:48:13 +0530

    Add files via upload
    Employee.cpp file

---
+++ Employee.cpp
@@ -0,0 +1,40 @@
+#include "Employee.h"
+
+Employee::Employee()
+{
+	EmpID = 0;
+	EmpName = "";
+	EmpEmail = "";
+}
+
+Employee::Employee(int Eid, string Ename, string E_mail)
+{
+	EmpID = Eid;
+	EmpName = Ename;
+	EmpEmail = E_mail;
+}
+
+void Employee::setEmployeeDetails(int pid, string pname, string pemail)
+{
+
+}
+
+void Employee::Login()
+{
+	
+}
+
+void Employee::IssuingTickets()
+{
+	
+}
+
+void Employee::CollectPayment()
+{
+	
+}
+
+Employee::~Employee()
+{
+	//Destructor
+}
